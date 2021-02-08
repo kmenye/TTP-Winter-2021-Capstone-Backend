1e1aa1ae62aa5cd8e5ee838f3403d0d76da4631f
Keith Menye <k.menye@gmail.com>
2021-02-07 20:02:19 -0500

    Add Auth Backend

--- index.js
+++ index.js
@@ -1,38 +1,76 @@
-//NODE MODULES
-const express = require('express');
-const morgan = require('morgan');
-const compression = require('compression');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const authRouter = require("./auth");
+const apiRouter = require("./routes");
+const cors = require("cors");
 
-//IMPORTS/VARIABLES
-const PORT = process.env.PORT || 8080;
-const db = require('./db');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const db = require("./database");
+const sessionStore = new SequelizeStore({ db });
 
 const app = express();
 
-//CORS!
-app.use(cors());
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findByPk(id);
+    done(null, user);
+  }
+  catch (err) {
+    done(err);
+  }
+});
 
-//Mount on API
-app.use('/api', require('./api'));
+const syncDb = async () => {
+  await db.sync({ force: true });
+}
 
-//START BACKEND SERVER FUNCTIOON
-const serverRun = () => {
-  const server = app.listen(PORT, () => {
-    console.log(`Live on port : ${PORT}`);
+const configureApp = () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+
+  /*
+  
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
   });
-};
-//DB Sync Function
-//Optional parameters
-// {force:true} - drops current tables and places new empty tables
-//{alter:true} - This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
 
-const syncDb = () => db.sync();
-// Connects to //postgres://localhost:5432/dbname
+  */
 
-//Run server and sync DB
-syncDb();
-serverRun();
+  app.use(
+    session({
+      secret: "a super secretive secret key string to encrypt and sign the cookie",
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false
+    })
+  );
 
-module.exports = app;
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use("/auth", authRouter);
+  app.use("/api", apiRouter);
+}
+
+const startListening = () => {
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}!!!`);
+  })
+}
+
+const bootApp = async () => {
+  await sessionStore.sync();
+  await syncDb();
+  await configureApp();
+  await startListening();
+}
+
+bootApp();
